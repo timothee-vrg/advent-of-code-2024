09df54846ad30234a049fcaba7019f45a6cee736
timothee-vrg <timothee.vargenau@free.fr>
2024-12-07 17:57:51 +0100

    add day 7

---
+++ 07/day7_2.py
@@ -0,0 +1,21 @@
+def is_accessible(numbers: list[int], goal: int) -> bool:
+    if len(numbers) == 1:
+        return numbers[0] == goal
+    
+    return is_accessible([numbers[0]+numbers[1]]+numbers[2:], goal) or is_accessible([numbers[0]*numbers[1]]+numbers[2:], goal) or is_accessible([int(str(numbers[0])+str(numbers[1]))]+numbers[2:], goal)
+
+def main():
+    res = 0
+    with open("07/input.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            goal_str, numbers_str = line.split(":")
+            goal = int(goal_str)
+            numbers = list(map(int,numbers_str.split()))  
+            if is_accessible(numbers,goal):
+                res+=goal
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
